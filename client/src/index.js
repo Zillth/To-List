ffc78ae18bfad50426554c093b6abbcda540a8df
Alan Flores <alanflores.r02@gmail.com>
2021-03-15 10:53:35 -0600

    theme created

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { theme } from './theme'
 
 ReactDOM.render(
   <>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </>,
   document.getElementById('root')
 );
---
+++ client/src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core'
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ffab73'
+        },
+        secondary: {
+            main: '#ffaec0'
+        },
+        containerDark: {
+            main: '#ffd384'
+        },
+        containerLight: {
+            main: '#fff9b0'
+        }
+    }
+})

6d5e3b8eb881e75fae10fdaa253748a6dbc89118
Alan Flores <alanflores.r02@gmail.com>
2021-03-29 20:17:31 -0600

    mongoDB connection done, envExample

--- server/index.js
+++ server/index.js
@@ -3,24 +3,26 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import userRouter from './routes/user.js'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express()
 dotenv.config()
 
 // Middlewares
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 // Routes
 app.use('/user', userRouter)
 app.use('/', (req, res) => {
-    res.status(200).json({message: "Welcome to the API of To-List project by Alan Jomar Flores Rodriguez"})
+    res.status(200).json({ message: "Welcome to the API of To-List project by Alan Jomar Flores Rodriguez" })
 })
 
 
-// Connection
+// Connection with DB
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`)
-})
+const CONNECTION_URL = process.env.CONNECTION_URL
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`)))
+    .catch(err => console.log(err))
